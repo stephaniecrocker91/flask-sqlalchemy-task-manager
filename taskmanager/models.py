167d3d6742ab6d2a369a25d8701344c6418723fd
Stephie <stephaniecrocker91@gmail.com>
2022-06-02 11:51:25 +0000

    basic front end set up with nav footer and and a tasks template using template inheritance to extend from base file

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -24,4 +24,6 @@ class Task(db.Model):
 
     def __repr__(self):
     # __repr__ to represent itself in the form of a string
-        return self.category_name
+        return "#{0} - Task: {1} | Urgent: {2}", format(
+            self.id, self.task_name, self.is_urgent
+        )
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // sidenive initialization
+    var sidenav= document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});

3ffbc20f7779d69a3155dde2658173ea9b56b07f
Stephie <stephaniecrocker91@gmail.com>
2022-06-02 11:17:38 +0000

    Pulled rebase true

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,7 +4,7 @@ class Category(db.Model):
     # schema for Category Model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
 
     def __repr__(self):
@@ -20,7 +20,7 @@ class Task(db.Model):
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id"ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
     # __repr__ to represent itself in the form of a string

4699cf39798a4962cac2a30f2270edcb107b8950
Stephie <stephaniecrocker91@gmail.com>
2022-06-03 08:04:19 +0000

    Add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     // sidenive initialization
-    var sidenav= document.querySelectorAll('.sidenav');
+    var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+
+    // datepicker initialization
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+    // select initialization
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
 });
